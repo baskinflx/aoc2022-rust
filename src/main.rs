97d8b18af95c1dbbf55d05a63568c585b2163fff
Baski Janarthanam <bjanarthanam@netflix.com>
2022-11-30 22:47:57 -0800

    AOC day 1 Elves sum

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod aocday1elf_top_sum;
+fn main() {
+    println!("--------------1---------------");
+    aocday1elf_top_sum::first();
+    println!("---------------2--------------");
+    aocday1elf_top_sum::second();
+    println!("-----------------------------");
+}
